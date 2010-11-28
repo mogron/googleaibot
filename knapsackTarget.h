913b304823e42de446dfc50d5c75b8174c32ecb9
Moritz Gronbach <meismo@arcor.de>
2010-11-28 12:10:01 +0100

    added knapsackTarget.h

---
+++ knapsackTarget.h
@@ -0,0 +1,14 @@
+#ifndef _KNAPSACKTARGET_H_
+#define _KNAPSACKTARGET_H_
+
+#include "planet.h"
+
+struct KnapsackTarget{
+  Planet p;
+  int t;
+  int weight;
+  int value;
+};
+
+
+#endif /* _KNAPSACKTARGET_H_ */
